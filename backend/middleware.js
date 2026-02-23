0120ffdc23ba5d508bfdaa505ef7a50c45d08e1c
Ayush <ayushgevariya8914@gmail.com>
2026-02-23 15:21:28 +0530

    feat: Auth Middleware

---
+++ backend/middleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("./db");
+
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(decoded && decoded.userID) {
+            const user = await User.findById(decoded.userID);
+            if(!user) {
+                return res.status(403).json({
+                    message: "Unauthorized"
+                })
+            }
+            req.user = user;
+        } 
+        next();
+    }
+    catch(err) {
+        return res.status(403).json({
+            message: "Unauthorized",
+            error: err.message
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
