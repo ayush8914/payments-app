2c059e689330defc6da98c94e0dd78762afdd2d6
Ayush <ayushgevariya8914@gmail.com>
2026-02-24 13:27:14 +0530

    fix: wiring BE APIs

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -5,13 +5,14 @@ import InputBox from './InputBox'
 import { Button } from './Button'
 import BottomWarning from './BottomWarning'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [userName, setuserName] = useState('');
   const [password, setPassword] = useState('');
-
+  const navigate = useNavigate();
   return (
     <div className='bg-slate-300 flex justify-center h-screen'>
         <div className='flex flex-col justify-center w-2/3 sm:w-1/5'>
@@ -23,7 +24,26 @@ function SignUp() {
                 <InputBox placeholder="100xdev@gmail.com" label={"Email"} value={userName} onChange={(e) => setuserName(e.target.value)} />
                 <InputBox placeholder="123456" label={"Password"} value={password} onChange={(e) => setPassword(e.target.value)} />
                 <Button label="Sign Up" onClick={async() => {
-                    console.log(firstName, lastName, userName, password);
+                    const response = await fetch('http://localhost:3000/api/v1/user/signup',{
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json'
+                      },
+                      body: JSON.stringify({
+                        first_name: firstName,
+                        last_name: lastName,
+                        userName: userName,
+                        password: password
+                      })
+                    })
+
+                    const data = await response.json();
+                    if(response.status !== 200) {
+                        console.log(data.message);
+                        return;
+                    }
+                    localStorage.setItem("token", data.token);   
+                    navigate("/dashboard");                 
                 }} />
                 <BottomWarning label="Already have an account?" buttonText="Sign In" to="/signin" />
             </div>
